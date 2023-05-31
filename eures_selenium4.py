d5e48c0dd9de44851df5124ecd0c84fc5a04bd17
shuchitaprakash03@gmail.com <shuchitaprakash03@gmail.com>
2023-05-31 23:32:16 +0200

    uncommemted

--- eures_selenium4.py
+++ eures_selenium4.py
@@ -8,18 +8,18 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.common.exceptions import StaleElementReferenceException
 
 
-# # start by defining the options 
-# options = webdriver.ChromeOptions() 
-# options.headless = True # it's more scalable to work in headless mode 
-# # normally, selenium waits for all resources to download 
-# # we don't need it as the page also populated with the running javascript code. 
-# options.page_load_strategy = 'none' 
-# # this returns the path web driver downloaded 
-# chrome_path = ChromeDriverManager().install() 
-# chrome_service = Service(chrome_path) 
+# start by defining the options 
+options = webdriver.ChromeOptions() 
+options.headless = True # it's more scalable to work in headless mode 
+# normally, selenium waits for all resources to download 
+#  we don't need it as the page also populated with the running javascript code. 
+ options.page_load_strategy = 'none' 
+#  this returns the path web driver downloaded 
+chrome_path = ChromeDriverManager().install() 
+chrome_service = Service(chrome_path) 
 # # pass the defined options and service objects to initialize the web driver 
-# driver = Chrome(options=options, service=chrome_service) 
-# driver.implicitly_wait(5)
+driver = Chrome(options=options, service=chrome_service) 
+driver.implicitly_wait(5)
 
 driver = Chrome(executable_path='/usr/local/bin/chromedriver')
 
